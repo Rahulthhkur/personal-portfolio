60ea101c2d4d7e3a7b996ead92570556cf73b246
Rahul Singh <rthakur1233@gmail.com>
2024-04-10 17:30:32 +0530

    design update

--- script.js
+++ script.js
@@ -1,47 +1,6 @@
-// Toggle icon and navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
-// Scroll section and highlight navbar links
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-            });
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-        }
-        sec.classList.add('show-animate');
-    });
-}
-
-// Sticky header
-window.onscroll = () => {
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-}
-
-// Remove toggle icon and navbar when clicking navbar links (scroll)
-menuIcon.classList.remove('bx-x');
-navbar.classList.remove('active');
-
-//remove toggle icon and navbar when click navbar links (scroll)
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        menuIcon.classList.remove('bx-x');
-        navbar.classList.remove('active');
-    });
-});
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+};
